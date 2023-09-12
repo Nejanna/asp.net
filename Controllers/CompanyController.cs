57f2adc18add39da7ef8905c4239464768e6c0a8
nejanna <nejanna288@gmail.com>
2023-09-12 18:15:02 +0300

    Add project files.

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,39 @@
+﻿using laboratory1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace laboratory1.Controllers
+{
+    //зміна обробки запитів
+    [ApiController]
+    [Route("company")]
+    public class CompanyController: ControllerBase
+    {
+        private readonly Random random = new Random();
+
+        
+        public IActionResult GetCompany()
+        {
+            // ініціалізація властивостей 
+            var company = new Company
+            {
+                YearOfCreation = 10,
+                NameCompany = "Unicorn Company",
+                Founder = "Nei Anna Vasylivna.",
+                Mission = "We offer the highest quality computers for people all over the world"
+            };
+            //завдання 2 
+            var randomNumber = random.Next(0, 101);
+            
+            var variableToDisplay = $"There are {randomNumber}customers on the site\n" +
+                $"Creation date: {company.YearOfCreation} years\n" +
+                                $"Company Name: {company.NameCompany}\n" +
+                                $"Founder: {company.Founder}\n" +
+                                $"Mission: {company.Mission}";
+
+
+            return Ok(variableToDisplay);
+        }
+
+    }
+}
---
+++ Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laboratory1.Models
+{
+    public class Company
+    {
+        //властивості
+        public int YearOfCreation { set; get; }
+        public string NameCompany { set; get; }
+        public string Founder { set; get; }
+        public string Mission { set; get; }
+    }
+}
